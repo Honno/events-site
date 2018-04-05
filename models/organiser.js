882054d584e16a1c6d8d69f336e1bea07c926ee3
Matthew Barber <quitesimplymatt@gmail.com>
2018-04-05 17:12:45 +0100

    prelim organiser schema

---
+++ models/organiser.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var OrganiserSchema = new Schema(
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    }
+    password: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    display_name: {
+        type: String,
+        required: true
+    }
+);
+
+var Organiser = mongoose.model('Organiser', OrganiserSchema);
+
+module.exports = Organiser;
